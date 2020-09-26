b974eb4763f824e63ac1bc66c8ff01b6cc23f0d1
Nile Walker <niwal7@morgan.edu>
2020-09-25 22:45:28 -0400

    Updated UI

--- Letter/Letter.ino
+++ Letter/Letter.ino
@@ -1,11 +1,6 @@
 #include <FastLED.h>
 #define NUM_LEDS 84
 
-#define TX_BUFFER_SIZE 32 // * 5 bytes
-unsigned int cmds_buffered = 0;
-
-#define READY2RECIEVE 8
-#define RECEIVER_READY 8
 #define DATA_PIN 3
 
 CRGB leds[NUM_LEDS];
@@ -16,18 +11,13 @@ void setup()
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 
   pinMode(DATA_PIN, OUTPUT);
-  pinMode(RECEIVER_READY, INPUT);
-  pinMode(READY2RECIEVE, OUTPUT);
 
   FastLED.setBrightness(BRIGHTNESS);
   Serial.begin(115200);
 }
 
-byte waiting_cmd_buffer[TX_BUFFER_SIZE][5]
-byte cmd_buffer[5];
 void loop()
 {
-  // NOTE: Increase buffer size to 256 in HardwareSerial.h
   if (Serial.available() >= 6) // Full Command is in buffer including start byte
   {
     if (Serial.read() == '#') // Check Start Byte
@@ -40,9 +30,7 @@ void loop()
       {
         if (cmd_buffer[1] == 255) // Check for screen refresh command
         {
-          digitalWrite(READY2RECIEVE, HIGH); // Not ready to recieve data 
-          FastLED.show(); // Refresh leds
-          digitalWrite(READY2RECIEVE, LOW);
+          FastLED.show();                    // Refresh leds
         }
         else
         {
@@ -51,20 +39,9 @@ void loop()
       }
       else
       {
-        if (digitalRead(RECEIVER_READY) == LOW) {
-          // Empty Buffered Commands
-          while (cmds_buffered > 0) {
-            Serial.print('#');
-            Serial.write(waiting_cmd_buffer[--cmds_buffered], 5);
-          }
-          Serial.print('#'); // Send Start Byte
-          cmd_buffer[0]--; // Decrement hops remaining
-          Serial.write(cmd_buffer, 5);
-        } else {
-          if (cmds_buffered < TX_BUFFER_SIZE){
-          waiting_cmd_buffer[cmds_buffered++] = cmd_buffer;
-          }
-        }
+        Serial.print('#'); // Send Start Byte
+        cmd_buffer[0]--;   // Decrement hops remaining
+        Serial.write(cmd_buffer, 5);
       }
     }
   }
--- Test/Test.ino
+++ Test/Test.ino
@@ -1,41 +1,27 @@
-byte cmd_buffer[6] = {'#', 0, 0, 255, 0, 255};
-byte cmd_buffer2[6] = {'#', 0, 0, 0, 0, 0};
+byte cmd_buffer[6] = {'#', 0, 0, 255, 0, 0};
 byte reset_buffer[6] = {'#', 0, 255, 255, 0, 0};
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
 }
 
-#define FRAME_RATE 20
-int cnts[4] = {151,57, 84, 43};
-void loop() {
+#define FRAME_RATE 30
+int cnts[4] = {151, 57, 84, 43};
+void loop()
+{
   // put your main code here, to run repeatedly:
-  for (int j = 0; j < 4; j++) {
+  for (int j = 3; j >= 0; j--)
+  {
     cmd_buffer[1] = j;
-    for (int i = 0; i < cnts[j]; i++) {
+    for (int i = 0; i < cnts[j]; i++)
+    {
       cmd_buffer[2] = i;
       Serial.write(cmd_buffer, 6);
     }
-  }
-  for (int j = 0; j < 4; j++) {
     reset_buffer[1] = j;
     Serial.write(reset_buffer, 6);
-    delay(20);
   }
-
-  delay((1000 / FRAME_RATE) / 2);
-  for (int j = 0; j < 4; j++) {
-    cmd_buffer2[1] = j;
-    for (int i = 0; i < cnts[j]; i++) {
-      cmd_buffer2[2] = i;
-      Serial.write(cmd_buffer2, 6);
-    }
-  }
-  for (int j = 0; j < 4; j++) {
-    reset_buffer[1] = j;
-    Serial.write(reset_buffer, 6);
-    delay(20);
-  }
-
-  delay((1000 / FRAME_RATE) / 2);
-} 
+  cmd_buffer[3] = cmd_buffer[3] == 0 ? 255 : 0; // Switch State
+  delay((1000 / FRAME_RATE));
+}
