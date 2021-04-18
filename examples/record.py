46728b529d2d7fa218a32392d6cee6258a3f9e92
NWalker4483 <niwal7@morgan.edu>
2021-04-18 16:15:13 -0400

    Rainbow Logic

--- examples/record.py
+++ examples/record.py
@@ -16,46 +16,71 @@ window_size = window.get_size()
 
 sign = LedSign.load("sign.txt")
 sign.attach("COM3")
-sign.recording = True
+
+sign.adjustable = True
 
 run = True
 ret = True
 frames = [] # Resizing frames gets really expensive so it might be better off
+
+ret, frame = cap.read() 
+frame = cv2.resize(frame, dsize=window_size[::-1], interpolation=cv2.INTER_NEAREST)
+frame -= frame % 100
+frames.append(frame)
 print("loading video")
-while True:
+start = False
+while run and ret:
     events = pygame.event.get()
+    try:
+        if start:
+            ret, frame = cap.read() 
+            ret, frame = cap.read() 
+            if ret:
+                frame = cv2.resize(frame, dsize=window_size[::-1], interpolation=cv2.INTER_NEAREST)
+                frame -= frame % 100
+                frames.append(frame)
+                pygame.surfarray.blit_array(window, frame)
+            else:
+                cap.release()
+                break       
+        else:
+            pygame.surfarray.blit_array(window, frame)
+        
+        sign.update(window, events)
+        sign.draw(window)
+        pygame.display.flip()
+    except Exception as e:
+        print(e)
+        break
+    
     for event in events:
         if event.type == pygame.QUIT:   
+            run = False
             break
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                run = False
                 break
-    try:
-        ret, frame = cap.read() 
-        ret, frame = cap.read() 
-        if ret:
-            frame = cv2.resize(frame, dsize=window_size[::-1], interpolation=cv2.INTER_NEAREST)
-            pygame.surfarray.blit_array(window, frame)
-            frames.append(frame)
-            sign.update(window, events)
-            sign.draw(window)
-            pygame.display.flip()
-        else:
-            cap.release()
-            break
-    except Exception as e:
-        break
+            if event.key == pygame.K_RIGHT:
+                ret = False
+                break
+            if event.key == pygame.K_DOWN:
+                sign.recording = True
+                start = True
+                break
 print("Playing Back")
 sign.recording = False
 sign.adjustable = False
 frame_data = zip(sign.record, frames)
-while True:
+while run == True:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:   
+            run = False
             break
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                run = False
                 break
     for cmd_data, frame in frame_data:
         clock.tick(60)
