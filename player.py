ee8db7b198fed8e23a7201358adfab8c62151e33
Nile Walker <nilezwalker@gmail.com>
2021-11-03 12:53:38 -0400

    Added otpimizations

--- player.py
+++ player.py
@@ -28,13 +28,16 @@ sign.attach("/dev/ttyUSB0")
 
 video_dir = "videos"
 video_index = 0 
+frame_index = -1 
+frame_cache = []
+cache_index = video_index
 video_files = [os.path.join(video_dir, i) for i in os.listdir(video_dir) if ".mp4" in i]
 video_handle = cv2.VideoCapture(video_files[video_index])
 loaded = True
+loop_done = False
 
 clock = pygame.time.Clock()
 is_running = True
-
 
 while is_running:  
     time_delta = clock.tick(24)/1000.0
@@ -59,20 +62,32 @@ while is_running:
         manager.process_events(event)
     
     ret, frame = video_handle.read() 
+    frame_index += 1
     if ret and loaded:
         frame = cv2.rotate(frame, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
         frame = cv2.resize(frame, dsize=window_size[::-1], interpolation=cv2.INTER_NEAREST)
         # frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
         # TODO: Delegate color space limiting to the LedSign object it makes more sense and limits unnecessary computation
         # frame -= frame % 100 # Reduce Collor Space and limit serial communication
-
         pygame.surfarray.blit_array(window_surface, frame)
-        
-        sign.sample_surface(window_surface)
+
+        if loop_done: # After a loop has been completed use cached sample results for each frame
+            changes = frame_cache[frame_index]
+        else:
+            changes = sign.sample_surface(window_surface, return_only=True)
+            frame_cache.append(changes)
+        for cmd in changes:
+            sign.send_cmd(*cmd)
 
         sign.draw(window_surface)
     else:
+        if video_index == cache_index:
+            loop_done = True
+        else: 
+            frame_cache = []
+            cache_index = video_index
         video_handle = cv2.VideoCapture(video_files[video_index])
+        frame_index = -1
         loaded = True
 
     manager.update(time_delta)
